77422f906ca42d8e6066fd2924e8c60faf684b41
yoshimune <seatangle.dash@gmail.com>
2015-02-04 23:36:27 +0900

    Add user_spec to downcase, format

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,10 @@ class User < ActiveRecord::Base
   validates(:name, presence: true, length: { maximum: 50 })
   
   # email
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates(:email, presence: true, format: { with: VALID_EMAIL_REGEX },
              uniqueness: { case_sensitive: false })    
-  before_save { self.email = email.downcase }
+  before_save { email.downcase! }
   
   # password
   has_secure_password
