573802134eaa1f83b2a2433439588b4148b5f9c9
yoshimune <seatangle.dash@gmail.com>
2015-03-07 14:55:46 +0900

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,10 @@ class SessionsController < ApplicationController
   end
   
   def create
-    user = User.find_by(email: params[:email].downcase)
-    if user && user.authenticate(params[:password])
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
       sign_in user
-      redirect_to user
+      redirect_back_or user
     else
       flash.now[:error] = 'Invalid email/password combination'
       render 'new'
@@ -15,6 +15,7 @@ class SessionsController < ApplicationController
   end
   
   def destroy
-    
+    sign_out
+    redirect_to root_url
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,6 +11,11 @@ module SessionsHelper
     !current_user.nil?
   end
   
+  def sign_out
+    self.current_user = nil
+    cookies.delete(:remember_token)
+  end
+  
   def current_user=(user)
     @current_user = user
   end
@@ -18,5 +23,18 @@ module SessionsHelper
   def current_user
     remember_token = User.encrypt(cookies[:remember_token])
     @current_user ||= User.find_by(remember_token: remember_token)
+  end
+  
+  def current_user?(user)
+    user==current_user
+  end
+  
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+  
+  def store_location
+    session[:return_to] = request.url
   end
 end
